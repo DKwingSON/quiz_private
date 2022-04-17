3a82371b7e676c959f01842623e8a4dac5aac3d1
dkwingson <429482349@qq.com>
2022-04-18 02:14:40 +0800

    fully functional quiz

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -4,131 +4,178 @@ var tScore = 0;
 var record = 0;
 var quizRec = "";
 var temp;
-function next(t){
+var quizIdx = 0; //当前索引
+var lockTimer = 0;
+var quizInfo = [
+    {
+        desc: "地球是圆的1？",
+        option: [
+            ["是", "1A"],
+            ["否", "0B"],
+            ["你说呢", "0C"],
+            ["钝角", "0D"],
+        ],
+        explain: "你真的需要解释吗？",
+    },
+    {
+        desc: "地球是圆的22？",
+        option: [
+            ["是", "1A"],
+            ["否", "0B"],
+            ["地球是什么？", "0C"],
+            ["钝角", "0D"],
+        ],
+        explain: "你真的需要解释吗？",
+    },
+];
+function isQuizDone() {
+    return quizIdx >= quizInfo.length;
+}
+function next() {
     $("div#bd > div.panel-body").hide();
-    $("div.js_answer").eq(t).show();
-    $("div.js_answer").eq(t).children("input").attr("checked","");
+    $("#quiz_list").find('li').find('input').each(function () {
+        $(this).attr("checked", "");
+    });
+    gotoTop();
+    resetTimer();
+    quizIdx = quizIdx + 1;
+    loadQuiz(quizIdx);
+}
+
+function resetTimer(){
+    $("#btn_next").hide();
+    $("#btn_next").hide();
+    $("#btn_lock").show();
+}
+
+function revert(){
+    quizIdx = quizIdx - 1;
+    next();
+}
+
+function start() {
+    quizIdx = 0;
+    tScore = 0;
+    quizInfo.forEach(quiz => {
+        shuffle(quiz.option);
+    });
+    shuffle(quizInfo);
+    loadQuiz(quizIdx);
+}
+
+function loadQuiz(id) {
+    if (isQuizDone()) {
+        result();
+    }
+    var q = quizInfo[id];
+    var idx = 0;
+    $("div#bd > div.panel-body").hide();
+    $("#panel_quiz").show();
+    $("#quiz_desc").text(quizInfo[id].desc);
+    $("#quiz_list").find('li').each(function () {
+        var option_desc = q.option[idx][0];
+        var option_value = q.option[idx][1];
+        var obj = '<input name="g" type="radio" class="" value="' + option_value + '"/>' + option_desc;
+        $(this).html(obj);
+        idx += 1;
+    });
+}
+
+function result(t) {
+    $("div#bd > div.panel-body").hide();
+    $("#panel_result").show();
     gotoTop();
 }
-function result(t){
+
+function explain(id, isAnsCorrect) {
+    var explain_desc = "回答错误，少侠请重新来过";
+    if(isAnsCorrect){
+        explain_desc = quizInfo[id].explain;
+        lockDown(function(){
+            $("#btn_next").show();
+            $("#btn_revert").hide();
+        }, 3 * 1000);
+    } else {
+        lockDown(function() {
+            $("#btn_next").hide();
+            $("#btn_revert").show();
+        }, 6 * 1000);
+    }
     $("div#bd > div.panel-body").hide();
-    
-    $("div.js_result").eq(fullScore - t).show();
-	$("div.js_result").eq(fullScore - t).find(".resultscore").eq(0).html($("#totalsc").val());
-	gotoTop();
+    $("#explain_head").text("问题#" + (id + 1));
+    $("#explain_desc").text(explain_desc);
+    setTimeout(function(){
+        $("#panel_explain").show();
+    }, 200);
+    return 0;
 }
 
-function push(){
-    // Variable to hold request
-	var request;
-
-	
-
-    // Abort any pending request
-    if (request) {
-        request.abort();
-    }
-    // setup some local variables
-    var $form = $("#collector");
-
-    $('<input />').attr('type', 'hidden')
-          .attr('name', "Total")
-          .attr('value', tScore)
-          .appendTo($form);
-          
-    for (i=1;i<11; i++){
-    
-      $('<input />').attr('type', 'hidden')
-          .attr('name', "A"+i)
-          .attr('value', quizRec[i-1])
-          .appendTo($form); 
-    }
-    
-    // Let's select and cache all the fields
-    var $inputs = $form.find("input, select, button, textarea");
-	temp = $form;
-    // Serialize the data in the form
-    var serializedData = $form.serialize();
-
-    // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
-    $inputs.prop("disabled", true);
-
-    // Fire off the request to /form.php
-    request = $.ajax({
-        url: "http://zwen668.com/git/foolsDayQuiz/src/php/post.php",
-        type: "post",
-        data: serializedData
-    });
-
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-    });
-
-    // Callback handler that will be called on failure
-    request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+
-            textStatus, errorThrown
-        );
-    });
-
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
-    request.always(function () {
-        // Reenable the inputs
-        $inputs.prop("disabled", false);
-    });
-
-    // Prevent default posting of form
-    event.preventDefault();
-}
-
-function toggle(t){
-	var child =  $(t).children("input");
-	if (child.length == 0){
-		var gender = document.getElementsByName("Gender")[0].value;
-		var faculty = document.getElementsByName("Faculty")[0].value;
-		var age = document.getElementsByName("Age")[0].value;
-		if (gender == "" | faculty =="" | age == ""){
-			alert("看结果前，请完善您的个人信息");
-		}else {
-			push();
-		
-			//Hard coded maximum,please do not try to hack it 
-			if (tScore > 10 ) {
-				tScore = 10
-			}
-			
-			modifyTitle(tScore);
-			result(tScore);
-		}
-	
-	}else {
-    	$(t).children("input").attr("checked","checked");
-  		$("li.list-group-item").removeClass('active')
-    	var rt = $(t).children("input:checked").val();
-    	var score = rt.match(/\d+/)[0];
-      	tScore  = parseInt(tScore) + parseInt(score);
-    	console.log("tScore => " + tScore);
-    	quizRec = quizRec + rt.match(/[a-zA-Z]/);
-		$("#totalsc").val(tScore);
-   		$(t).addClass('active');
-    	var t = $("div.js_answer").index($(t).parents("div.js_answer")) + 1;
-    	if(t == total){
-        	push();
-		    modifyTitle(tScore);
-        	result(tScore);
-    	}else{
-        	setTimeout(function(){next(t);},500);
-    	}
+//toggle callback
+function toggle(t) {
+    if (isQuizDone()) {
+        console.log("done...");
+        result();
+        return;
+    } else {
+        $(t).children("input").attr("checked", "checked");
+        $("li.list-group-item").removeClass('active')
+        var rt = $(t).children("input:checked").val();
+        var score = rt.match(/\d+/)[0];
+        console.log("tScore => " + tScore + ",score => " + score);
+        if (score <= 0) {
+            explain(quizIdx, false);
+        } else {
+            tScore = parseInt(tScore) + parseInt(score);
+            explain(quizIdx, true);
+        }
     }
 }
 
-function gotoTop(){
-    $("body,html").animate({scrollTop:($("#header").offset().top + $("#header").height())}, 1000);
+//time is the seconds of lock down duration
+function lockDown(func, duration) {
+    if (typeof duration != "number") {
+        alert("网络繁忙，请稍后再试");
+        return;
+    } else {
+        var sTime = Number(new Date());
+        var eTime = sTime + duration;
+        clearInterval(lockTimer);
+        lockTimer = setInterval(() => {
+            var curTime = Number(new Date());
+            var left = ((eTime - curTime) / 1000);
+            if(left >= 0.0001){
+                $("#btn_next").hide();
+                $("#btn_revert").hide();
+                $("#btn_lock").text("请等待" + Math.floor(left) + "秒"); 
+            } else {
+                $("#btn_lock").hide();
+                clearInterval(lockTimer);
+                if(typeof func == "function"){
+                    func();
+                }
+            }
+        }, 100);
+    }
+}
+
+function gotoTop() {
+    $("body,html").animate({ scrollTop: ($("#header").offset().top + $("#header").height()) }, 1000);
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
 }
